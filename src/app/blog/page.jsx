10c81ac191c28d262fc429a71942efd7fe65a153
Ethan Lee <ethan.lee@emory.edu>
2024-07-16 21:39:21 +0900

    Add blog page placeholder

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,5 +1,11 @@
 const Blog = () => {
-  return <main>Blog</main>;
+  return (
+    <main>
+      <div className="w-full h-[calc(100vh-60px)] flex items-center justify-center">
+        <h4>Blog Coming Soon!</h4>
+      </div>
+    </main>
+  );
 };
 
 export default Blog;
